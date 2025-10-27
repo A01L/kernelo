41b0ac5cbd62f78ea8b97b3fe6ebb165b73610bf
Nuradil Takhirov <adil19032004@gmail.com>
2025-10-27 12:37:06 +0500

    release prototype v3.1.3

---
+++ core/setup.js
@@ -0,0 +1,49 @@
+#!/usr/bin/env node
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import { execSync } from 'child_process';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const targetDir = process.cwd();
+
+console.log(`Setup kernelo to : ${targetDir}`);
+
+function copyRecursive(src, dest) {
+    const stats = fs.statSync(src);
+    if (stats.isDirectory()) {
+        const baseName = path.basename(src);
+
+        if (['.git', 'node_modules'].includes(baseName)) return;
+
+        if (!fs.existsSync(dest)) fs.mkdirSync(dest);
+        for (const file of fs.readdirSync(src)) {
+            copyRecursive(path.join(src, file), path.join(dest, file));
+        }
+    } else {
+        fs.copyFileSync(src, dest);
+    }
+}
+
+copyRecursive(path.join(__dirname, '..'), targetDir);
+
+const oldPackage = path.join(targetDir, 'package.json');
+if (fs.existsSync(oldPackage)) {
+    fs.unlinkSync(oldPackage);
+}
+
+const tpPackage = path.join(targetDir, 'tp.json');
+if (fs.existsSync(tpPackage)) {
+    fs.renameSync(tpPackage, path.join(targetDir, 'package.json'));
+} else {
+    console.warn("tp.json not found ERRC: Sx001");
+}
+
+console.log("init (npm link) for cli kernelo...");
+execSync('npm link', { cwd: targetDir, stdio: 'inherit' });
+
+console.log("Success! You can use CLI of kernelo:");
+console.log("  kernelo start | kernelo help");
